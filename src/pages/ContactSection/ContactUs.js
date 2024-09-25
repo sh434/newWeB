7b0d61ce85403932546d22965e60d08c1715c274
shikha kumari <sikhakumarijsr01@gmail.com>
2024-09-25 18:26:10 +0530

    acd

--- src/pages/ContactSection/ContactUs.js
+++ src/pages/ContactSection/ContactUs.js
@@ -67,12 +67,14 @@ const ContactUs = () => {
                                     data-aos-duration={1200}
                                     data-aos-delay={150}
                                 >
-                                    <iframe
-                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.546467503306!2d-0.12209412307005911!3d51.50318971101062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604b900d26973%3A0x4291f3172409ea92!2slastminute.com%20London%20Eye!5e0!3m2!1sen!2sbd!4v1700026532111!5m2!1sen!2sbd"
+                    
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2480.228449761564!2d77.08505729846522!3d28.46229428043085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sC-70A%2C%207th%20Floor%2C%20Supermart-1%2C%20DLF%20phase%204%2C%20Gurgaon%2C%20Haryana%20-%20122002%20%209311254752%20%20info%40wpmindia.com%20%20Facebook%20%20Twitter%20%20Instagram%20%20Skype!5e0!3m2!1sen!2sin!4v1727268092850!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    {/* <iframe
+                                        src="https://maps.app.goo.gl/Cc9ffoG5QZdQSPVw5"
                                         allowFullScreen=""
                                         loading="lazy"
                                         referrerPolicy="no-referrer-when-downgrade"
-                                    />
+                                    /> */}
                                 </div>
                             </div>
 
@@ -92,8 +94,9 @@ const ContactUs = () => {
                                     <div className="contact__us--info__content">
                                         <h3 className="contact__us--info__title">Mail address</h3>
                                         <p className="contact__us--info__text">
-                                            <a href="mailto:info@example.com">info@example.com</a> <br />
-                                            <a href="tel:+998757478492">+998757478492</a>
+                                            <a href="mailto:info@example.com">
+                                                info@wpmindia.com</a> <br />
+
                                         </p>
                                     </div>
                                 </div>
@@ -123,7 +126,7 @@ const ContactUs = () => {
                                     <div className="contact__us--info__content">
                                         <h3 className="contact__us--info__title">Office address</h3>
                                         <p className="contact__us--info__text desc">
-                                            Digital Agency Network 2021 Eastbourne Terrace
+                                            C-70A, 7th Floor, Supermart-1, DLF phase 4, Gurgaon, Haryana - 122002
                                         </p>
                                     </div>
                                 </div>
@@ -153,9 +156,9 @@ const ContactUs = () => {
                                     <div className="contact__us--info__content">
                                         <h3 className="contact__us--info__title">Phone Number</h3>
                                         <p className="contact__us--info__text">
-                                            <a href="tel:+998757478492">+880 123 456 789 , 9987574</a>{" "}
+                                            <a href="tel:+998757478492">9311254752</a>{" "}
                                             <br />
-                                            <a href="tel:+998757478492">+880 123 456 789 </a>
+                                            {/* <a href="tel:+998757478492">+880 123 456 789 </a> */}
                                         </p>
                                     </div>
                                 </div>
--- src/pages/Properties/PropertiesDetail.js
+++ src/pages/Properties/PropertiesDetail.js
@@ -502,8 +502,7 @@ const PropertiesDetail = () => {
                         <details>
                           <summary>Open on Google Maps</summary>
                           <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20227915.86434928!2d1.2189515269861546!3d38.76296158058813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6f73e994d3fb5891%3A0x783ff0a076fdb99!2sCosing%20Connect%20Ltd%2C%20United%20Kingdom!5e0!3m2!1sen!2sbd!4v1699676848803!5m2!1sen!2sbd"
-                            allowFullScreen=""
+                            src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2480.228449761564!2d77.08505729846522!3d28.46229428043085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sC-70A%2C%207th%20Floor%2C%20Supermart-1%2C%20DLF%20phase%204%2C%20Gurgaon%2C%20Haryana%20-%20122002%20%209311254752%20%20info%40wpmindia.com%20%20Facebook%20%20Twitter%20%20Instagram%20%20Skype!5e0!3m2!1sen!2sin!4v1727268092850!5m2!1sen!2sin"
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
                           />
